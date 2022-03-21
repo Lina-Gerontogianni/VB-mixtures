570511db3837bb30178788b704da756440db0ebe
Lina Gerontogianni <32386975+LinaGer94@users.noreply.github.com>
2022-03-21 17:54:18 +0000

    main VB-DPPM algorithm

--- Poisson_Mixtures/VB-DPPM.R
+++ Poisson_Mixtures/VB-DPPM.R
@@ -152,7 +152,7 @@ pel <- pel + theme(
   axis.title.y=element_text(size=16,face="bold", colour = "royalblue"),
   plot.title = element_text(size=14,face="bold") 
 )
-print(pel)
+print(pel) 
 
 # final component weights (discard those with weight < 0.003)
 var_weights <- model_dppois$pi_M[model_dppois$pi_M>0.003]
